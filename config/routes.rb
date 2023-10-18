f5db5de08b1916c8138f66aac532bb663d40bf23
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2023-10-18 15:32:24 -0500

    Create the basic routes for all items under the api/v1/ namespacing

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,15 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  namespace :api do
+    namespace :v1 do
+      resources :transactions
+      resources :merhcants
+      resources :items
+      resources :invoices
+      resources :invoice_items
+      resources :customers
+    end
+  end
 end
