2fcefc39d0f6f2e69b1f27168d30e00059dfa7ee
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2023-10-18 22:43:00 -0500

    Modify the routes for the different endpoints temporarily

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,12 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-
   namespace :api do
     namespace :v1 do
-      resources :transactions
-      resources :merhcants
+      resources :merchants
       resources :items
-      resources :invoices
-      resources :invoice_items
-      resources :customers
+      # resources :transactions
+      # resources :invoices
+      # resources :invoice_items
+      # resources :customers
     end
   end
 end
