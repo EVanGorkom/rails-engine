8e7f991a9a7c8693c00ecc6037cb95908491acb5
Ethan Van Gorkom <ethan.vangorkom@gmail.com>
2023-10-19 02:07:16 -0500

    Create the create method for the ItemsController and the item_params private method, and added the create method spec test

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,11 +1,19 @@
 class Api::V1::ItemsController < ApplicationController
   def index
-    @items = Item.all
-    render json: @items
+    render json: Item.all
   end
 
   def show
-    @item = Item.find(params[:id])
-    render json: @item
+    render json: Item.find(params[:id])
+  end
+
+  def create
+    render json: Item.create(item_params)
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :unit_price, :merchant_id)
   end
 end
--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe "Items API endpoint" do
-  it "sends a list of all Items" do
+  it "This endpoint, sends a list of all Items" do
     merchant = Merchant.create(name: "Bob's Burgers")
     create_list(:item, 6)
 
@@ -10,12 +10,28 @@ describe "Items API endpoint" do
     expect(response).to be_successful
   end
 
-  it "sends a single item with a specified id" do
+  it "This endpoint, can get one item by its id" do
     merchant = Merchant.create(name: "Bob's Burgers")
     create_list(:item, 6)
     item = Item.create(name: "Burger", description: "Yummy", unit_price: 10.0, merchant_id: merchant.id)
 
     get "/api/v1/items/#{item.id}"
+
+    expect(response).to be_successful
+  end
+
+  it "This endpoint, can create a new item" do
+    merchant = Merchant.create(name: "Bob's Burgers")
+    item_params = ({
+                    name: "Burger",
+                    description: "Yummy",
+                    unit_price: 10.0,
+                    merchant: merchant.id
+    })
+    headers = {"CONTENT_TYPE" => "application/json"}
+
+    post "/api/v1/items", headers: headers, params: JSON.generate(item: item_params)
+    created_item = Item.last
 
     expect(response).to be_successful
   end
